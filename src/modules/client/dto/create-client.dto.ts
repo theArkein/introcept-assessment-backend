2c2976e5e9b009afa915063c699041efe2926a79
Sarad Singh <thearkein@Sarads-MacBook-Air.local>
2021-07-27 12:54:23 +0545

    create client validations added

--- src/modules/client/dto/create-client.dto.ts
+++ src/modules/client/dto/create-client.dto.ts
@@ -1,1 +1,48 @@
-export class CreateClientDto {}
+import { IsDate, IsDateString, IsEmail, IsEnum, IsNotEmpty, IsNumberString, IsString, Length } from 'class-validator';
+import { Gender } from '../../../enum/gender.enum';
+import { EducationLevel } from '../../../enum/education-level.enum';
+import { ContactMode } from '../../../enum/contact-mode.enum';
+import { Nationality } from '../../../enum/nationality.enum';
+
+export class CreateClientDto {
+    @IsNotEmpty()
+    @IsString()
+    firstname: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastname: string;
+
+    @IsNotEmpty()
+    @IsEnum(Gender)
+    gender: string;
+
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(10)
+    phone: string
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    address: string;
+
+    @IsNotEmpty()
+    @IsEnum(Nationality)
+    nationality: string;
+
+    @IsNotEmpty()
+    @IsDateString()
+    dob: string;
+
+    @IsNotEmpty()
+    @IsEnum(EducationLevel)
+    educationLevel: string;
+
+    @IsNotEmpty()
+    @IsEnum(ContactMode)
+    contactMode: string;
+}
