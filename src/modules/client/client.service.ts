bef0e0bf721421ed459e1c4f68ae57184392a6a6
Sarad Singh <thearkein@Sarads-MacBook-Air.local>
2021-07-27 12:17:54 +0545

    client storage connected to client service

--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { read, save } from "../../helpers/storage.helper";
+
+const storagePath = "src/modules/client/client.storage.csv"
 
 @Injectable()
 export class ClientService {
   create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+    let result = save(storagePath, createClientDto);
+    if (result['error']) {
+      return {
+        success: "false",
+        message: "Couldn't save client"
+      }
+    } else {
+      return {
+        success: "true",
+        data: result['data']
+      }
+    }
   }
 
   findAll() {
-    return `This action returns all client`;
+    let result = read(storagePath);
+    if (result['error']) {
+      return {
+        success: "false",
+        message: "Couldn't save client"
+      }
+    } else {
+      return {
+        success: "true",
+        data: result['data']
+      }
+    }
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} client`;
+    return {
+      success: "false",
+      message: "This feature is not available yet"
+    };
   }
 
   update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+    return {
+      success: "false",
+      message: "This feature is not available yet"
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} client`;
+    return {
+      success: "false",
+      message: "This feature is not available yet"
+    };
   }
 }
